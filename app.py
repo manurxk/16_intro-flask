5daaeb5f86550095f80fcf23fa50e094e6501657
gallopelado <juanftp100@gmail.com>
2024-08-13 19:24:20 -0400

    feat: jugando con el post y request

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -14,6 +14,12 @@ def contacto():
 @app.route('/contacto2')
 def contacto2():
     return render_template('contacto.html')
+
+@app.route('/guardar-mascota', methods=['POST'])
+def guardarMascota():
+    print(request.form)
+    nombreMascota = request.form.get('txtNombreMascota')
+    return f"Ya llego tu mascota <strong>{nombreMascota}</strong> al servidor"
 
 # se pregunta por el proceso principal
 if __name__=='__main__':
