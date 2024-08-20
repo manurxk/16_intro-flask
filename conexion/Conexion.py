e86e39a3c060d806b0b04d7ce03ab4af762877d9
Manurxk <armanuramirez16@gmail.com>
2024-08-20 18:54:08 -0400

    sciudades

--- conexion/Conexion.py
+++ conexion/Conexion.py
@@ -5,7 +5,7 @@ class Conexion:
     """Metodo constructor
     """
     def __init__(self):
-        self.con = psycopg2.connect("dbname=veterinaria-db user=juandba password=admin")
+        self.con = psycopg2.connect("dbname=veterinaria-db user=postgres host=localhost password=1873")
 
     """getConexion
 
--- dao/CiudadDao.py
+++ dao/CiudadDao.py
@@ -1,20 +1,58 @@
 # Data access object - DAO
+from flask import current_app as app
 from conexion.Conexion import Conexion
-
 class CiudadDao:
-
+    
     def getCiudades(self):
-
         ciudadSQL = """
         SELECT id, descripcion
         FROM ciudades
         """
-        # objeto conexion
+        #objeto conexion
         conexion = Conexion()
         con = conexion.getConexion()
+        cur = con.cursor()
         try:
-            pass
-        except:
-            pass
+            cur.execute(ciudadSQL)
+            #trae datos de db
+            lista_ciudades = cur.fetchall()
+            #retorno de datos
+            return lista_ciudades
+        except con.Error as e:
+            app.logger.info.info()
         finally:
-            pass
+            
+            cur.close()
+            con.close()
+
+    def guardarCiudad(self, descripcion):
+
+        insertCiudadSQL = """
+        INSERT INTO ciudades(descripcion) VALUES(%s)
+        """
+
+        conexion = Conexion()
+        con = conexion.getConexion()
+        cur = con.cursor()
+
+        #Ejecucion exitosa
+        try:
+            cur.execute(insertCiudadSQL, (descripcion,))
+            #se confirma la isercion
+            con.commit()
+
+            return True
+
+        #si algo falla aqui
+        except con.Error as e:
+            print(e)
+
+        #siempre se va a ejecutar
+        finally:
+            cur.close()
+            con.close()
+
+        return False    
+
+
+
